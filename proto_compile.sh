e0ec045bc88b6ad4e8840af786fc602dfe13d73e
“vijnasu” <hemanthjois@gmail.com>
2024-02-19 11:36:45 +0530

    protoc updates

--- proto_compile.sh
+++ proto_compile.sh
@@ -3,9 +3,11 @@
 # Navigate to the Protobuf directory
 cd protobuf
 
-# Compile Protobuf files
-protoc --python_out=../src/telemetry_access_controller/generated \
-       --grpc_python_out=../src/telemetry_access_controller/generated \
-       telemetry.proto telemetry_service.proto
+# Compile Protobuf files, including the path to the well-known types
+
+python3 -m grpc_tools.protoc \
+--proto_path=. \
+--python_out=../src/telemetry_access_controller/generated \
+--grpc_python_out=../src/telemetry_access_controller/generated tac.proto tac_service.proto
 
 echo "Protobuf compilation completed."
