376361fd1e3e94c9233a8c7ac16de3913667af4d
“vijnasu” <hemanthjois@gmail.com>
2024-02-19 11:22:54 +0530

    Dir restructure

--- src/telemetry_access_controller/telemetry_access_controller_impl.py
+++ src/telemetry_access_controller/telemetry_access_controller_impl.py
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-# Navigate to the Protobuf directory
-cd protobuf
-
-# Compile Protobuf files
-protoc --python_out=../src/telemetry_access_controller/generated \
-       --grpc_python_out=../src/telemetry_access_controller/generated \
-       telemetry.proto telemetry_service.proto
-
-echo "Protobuf compilation completed."
+# ToDo: Coding
